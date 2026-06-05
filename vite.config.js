97507ada53a55e8ba79464e8cb83aa6145780635
AdrijusC <263057910+Adrijus-code@users.noreply.github.com>
2026-06-05 15:44:10 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Quiz_app_repo/', 
+  base: '/Quiz_app/', 
   build: {
     outDir: 'docs', // Tells Vite to put files in a folder named docs
   }
